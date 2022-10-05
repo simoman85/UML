2f91715bb42c1d9fa94d21060d6ef95a3d76654b
simoman85 <114956802+simoman85@users.noreply.github.com>
2022-10-04 23:13:22 -0400

    Create InsufficientFundsException.java

---
+++ InsufficientFundsException.java
@@ -0,0 +1,13 @@
+package BankingSystem;
+/**
+ *
+ * @author mk4022
+ */
+
+   public class InsufficientFundsException extends Exception {
+    public InsufficientFundsException(String error) {
+    super(error);
+}
+}
+    
+
